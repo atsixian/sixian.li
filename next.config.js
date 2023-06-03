569b45c975d63576de5b9058470394bf9cbbe897
Sixian Li <lisixian579@gmail.com>
2023-06-03 11:24:41 +0800

    perf: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')()
-const { withContentlayer } = require('next-contentlayer')
+const withPlugins = require('next-compose-plugins')
+const mdx = require('@next/mdx')()
+const { withContentlayer: contentlayer } = require('next-contentlayer')
+const bundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = { reactStrictMode: true, swcMinify: true }
 
-module.exports = withContentlayer(withMDX(nextConfig))
+module.exports = withPlugins([mdx, contentlayer, bundleAnalyzer], nextConfig)
