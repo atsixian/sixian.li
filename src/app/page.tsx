930833d2e930a10c0b2ee54567a500b8fac5fe8e
Sixian Li <lisixian579@gmail.com>
2023-06-10 12:08:40 +0800

    chore: update license

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Me from './me.mdx'
+import NextLink from 'next/link'
 
 export default function Home() {
   return (
     <article className="prose prose-zinc dark:prose-invert prose-h1:mb-5">
       <Me />
+      <p className='text-sm text-fg-secondary-color'>
+        <NextLink href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+          CC BY-NC-SA 4.0
+        </NextLink>{' '}
+        2019-PRESENT © Sixian Li
+      </p>
     </article>
   )
 }
