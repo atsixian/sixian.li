a856de84aabaf247aa1b7a43180fcafe21d52cc5
Sixian Li <lisixian579@gmail.com>
2023-06-16 15:17:43 +0800

    refactor: recreate with framer motion

--- src/components/floating-nav.tsx
+++ src/components/floating-nav.tsx
@@ -1,83 +1,141 @@
-'use-client'
-
-import clsx from 'clsx'
+'use client'
+import { motion, Variants } from 'framer-motion'
 import { ChevronUp } from 'lucide-react'
 import NextLink from 'next/link'
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { NAV_LINKS, SOCIAL_LINKS } from './sidebar'
 
-export function FloatingNav() {
-  const [checked, setChecked] = useState(false)
+const navItemVariants: Variants = {
+  open: {
+    x: '0',
+    opacity: '100%',
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+    },
+  },
+  closed: {
+    x: '30vw',
+    opacity: '0%',
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+    },
+  },
+}
 
-  const delay = (order: number) => order * 50 + (checked ? 100 : 0)
+const arrowVariants: Variants = {
+  open: { rotate: 180 },
+  closed: { rotate: 0 },
+}
+
+const blurBgVariants: Variants = {
+  open: {
+    clipPath: 'circle(140% at 95% 95%)',
+    opacity: '100%',
+    transition: {
+      opacity: {
+        duration: 0.5,
+      },
+    },
+  },
+  closed: {
+    clipPath: 'circle(0% at 95% 95%)',
+    opacity: '0%',
+    transition: {
+      opacity: {
+        duration: 0.5,
+      },
+    },
+  },
+}
+
+export function FloatingNav() {
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="md:hidden">
-      <div
-        className={clsx(
-          'fixed inset-0 z-[98] h-full w-full bg-bg-color/80 backdrop-blur-md ease-in-out',
-          checked ? 'opacity-100' : 'opacity-50',
-          checked
-            ? '[transition:clip-path_400ms,opacity_400ms_100ms]'
-            : // on leaving, list items leave first
-              '[transition:clip-path_400ms_200ms,opacity_700ms_100ms]'
-        )}
-        style={{
-          clipPath: checked
-            ? `circle(150% at 95% 95%)`
-            : 'circle(0% at 95% 95%)',
+    <motion.div
+      className="md:hidden"
+      initial={false}
+      animate={isOpen ? 'open' : 'closed'}
+      variants={{
+        open: { transition: { staggerChildren: 0.1 } },
+        closed: { transition: { staggerChildren: 0.1, staggerDirection: -1 } },
+      }}
+    >
+      <NavButton isOpen={isOpen} setIsOpen={setIsOpen} />
+      <BlurBg />
+      <NavItems />
+    </motion.div>
+  )
+}
+
+function NavButton({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+}) {
+  return (
+    <div className="fixed bottom-4 right-4 z-[99] flex h-10 w-10 items-center justify-center rounded-full bg-zinc-800 drop-shadow-md">
+      <input
+        id="menu"
+        type="checkbox"
+        checked={isOpen}
+        onChange={() => setIsOpen(prev => !prev)}
+        className="appearance-none"
+      />
+      <motion.label
+        htmlFor="menu"
+        className="text-zinc-100"
+        variants={arrowVariants}
+        style={{ originY: 0.55 }}
+      >
+        <ChevronUp strokeWidth={1} size={28} />
+      </motion.label>
+    </div>
+  )
+}
+
+function BlurBg() {
+  return (
+    <motion.div
+      className="fixed inset-0 z-[98] h-full w-full bg-bg-color/80 backdrop-blur-md"
+      variants={blurBgVariants}
+    ></motion.div>
+  )
+}
+
+function NavItems() {
+  return (
+    <nav className="fixed bottom-20 right-7 z-[99]">
+      <motion.ul
+        className="flex flex-col items-end gap-3"
+        variants={{
+          open: { transition: { staggerChildren: 0.05 } },
+          closed: {
+            transition: { staggerDirection: -1, staggerChildren: 0.05 },
+          },
         }}
       >
-        <nav className="absolute bottom-20 right-7 z-[99]">
-          <ul className="flex flex-col items-end gap-3">
-            {NAV_LINKS.map((link, idx) => (
-              <li
-                key={link.href}
-                className={clsx(
-                  'transition-transform duration-500 ease-in-out',
-                  checked ? 'translate-x-0 opacity-100' : 'translate-x-[30vw]'
-                )}
-                style={{
-                  transitionDelay: `${delay(idx)}ms`,
-                }}
-              >
-                <NextLink href={link.href} className="text-2xl">
-                  {link.node}
-                </NextLink>
-              </li>
-            ))}
-          </ul>
-          <ul
-            className={clsx(
-              'mt-3 flex gap-2',
-              'transition-transform duration-500 ease-in-out',
-              checked ? 'translate-x-0 opacity-100' : 'translate-x-[50vw]'
-            )}
-            style={{
-              transitionDelay: `${delay(NAV_LINKS.length)}ms`,
-            }}
-          >
-            {SOCIAL_LINKS.map((link, idx) => (
-              <li key={link.href}>{link.node}</li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-      <div className="fixed bottom-4 right-4 z-[99] flex h-10 w-10 items-center justify-center rounded-full bg-zinc-800 drop-shadow-md">
-        <input
-          id="menu"
-          type="checkbox"
-          checked={checked}
-          onChange={() => setChecked(prev => !prev)}
-          className="peer/menu appearance-none"
-        />
-        <label
-          htmlFor="menu"
-          className="text-zinc-100 transition-transform peer-checked/menu:rotate-180"
-        >
-          <ChevronUp strokeWidth={1} size={28} />
-        </label>
-      </div>
-    </div>
+        {NAV_LINKS.map(link => (
+          <motion.li key={link.href} variants={navItemVariants}>
+            <NextLink href={link.href} className="text-2xl tracking-tight">
+              {link.node}
+            </NextLink>
+          </motion.li>
+        ))}
+        <motion.div variants={navItemVariants} className="mt-3 flex gap-2">
+          {SOCIAL_LINKS.map(link => (
+            <li key={link.href}>
+              <NextLink href={link.href} target="_blank">
+                {link.node}
+              </NextLink>
+            </li>
+          ))}
+        </motion.div>
+      </motion.ul>
+    </nav>
   )
 }
