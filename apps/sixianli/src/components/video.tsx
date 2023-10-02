43d1f629a19f4e630f0899a42593c668671ca59d
Sixian Li <lisixian579@gmail.com>
2023-10-02 14:17:51 +0800

    style: Video component

--- apps/sixianli/src/components/video.tsx
+++ apps/sixianli/src/components/video.tsx
@@ -4,14 +4,23 @@ export function Video({
   src,
   caption,
   className,
+  style,
 }: {
   src: string
   caption?: string
   className?: string
+  style?: React.CSSProperties
 }) {
   return (
     <div className="flex flex-col items-center">
-      <video autoPlay loop muted playsInline className={clsx('m-0', className)}>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className={clsx('m-0 rounded-md', className)}
+        style={style}
+      >
         <source src={src} type="video/mp4" />
       </video>
       <p className="mt-0 text-sm text-fg-secondary-color">{caption}</p>
