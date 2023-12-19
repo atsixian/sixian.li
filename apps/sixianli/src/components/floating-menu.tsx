d110f540120dce9b0756bd5c097cf214f34f1fcb
Sixian Li <lisixian579@gmail.com>
2023-12-19 23:01:54 +0800

    refactor: remove unused effects

--- apps/sixianli/src/components/floating-menu.tsx
+++ apps/sixianli/src/components/floating-menu.tsx
@@ -1,7 +1,6 @@
 'use client'
 import type { Variants } from 'framer-motion'
 import { motion } from 'framer-motion'
-import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export function FloatingMenu({
@@ -12,11 +11,6 @@ export function FloatingMenu({
   icon: React.ReactNode
 }) {
   const [isOpen, setIsOpen] = useState(false)
-  const pathname = usePathname()
-
-  useEffect(() => {
-    setIsOpen(false)
-  }, [pathname])
 
   useEffect(() => {
     if (isOpen) {
